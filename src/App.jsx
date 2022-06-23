f192143c5c47d1a29501c6b0f537651c80ad220e
Takaharayuuki <54837280+Takaharayuuki@users.noreply.github.com>
2022-06-23 09:20:22 +0900

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColofulMessage from "./components/ColofulMessage";
 
 const App = () => {
   const onClickButton = () => {
-    alert();
+    setNum(num + 1);
   };
+  const [num, setNum] = useState(0);
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColofulMessage color="blue" message="testetst" />
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
